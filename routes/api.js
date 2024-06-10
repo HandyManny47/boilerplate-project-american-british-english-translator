3827e2a7b90a9b4fc2c1f233525bc4be1988adc7
Manny Diaz <mannyd47@gmail.com>
2024-06-10 23:47:34 +0000

    update bugs

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,41 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+      const locale = req.body.locale;
+      const text = req.body.text;
+
+      if(text === undefined || !locale) {
+        return res.json({ error: 'Required field(s) missing' });
+      };
+
+      if (text.trim() === '') {
+        return res.json({ error: 'No text to translate' });
+      };
+      
+      if (locale === 'british-to-american') {
+        let translation = translator.britishToAmerican(text);
+        
+        if (text === translation){
+          return res.json({ text: text, translation: 'Everything looks good to me!'})
+        } else {
+          return res.json({ text: text, translation: translator.addSpanElement(text, translation) });
+        };        
+      };
+      
+      if (locale === 'american-to-british') {
+        let translation = translator.americanToBritish(text);
+
+        if (text === translation){
+          return res.json({ text: text, translation: 'Everything looks good to me!'})
+        } else {
+          return res.json({ text: text, translation: translator.addSpanElement(text, translation) });
+        };
+      };
+
+      res.json({ error: 'Invalid value for locale field' });
       
     });
 };
+      
+    
+
